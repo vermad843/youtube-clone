12212ec0bd55a2b1ff43b0bd594bcab6713fb8b7
vermad843 <vishnuverma7524@gmail.com>
2021-04-05 00:23:51 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
+    <div className="pp">
        <h1>Hello World</h1>
+       <Header/>  
     </div>
   );
 }
