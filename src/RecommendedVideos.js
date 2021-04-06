88f9361b3c31a9d1cdf24495eb7db7ed68d9844a
vermad843 <vishnuverma7524@gmail.com>
2021-04-06 10:27:56 +0530

    SidebarRow

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './RecommendedVideos.css'
+
 
 function RecommendedVideos() {
     return (
-        <div>
+        <div className = "recommendedVideos">
             <h1>I am RecommendedVideos</h1>
         </div>
-    )
+    );
 }
 
-export default RecommendedVideos
+export default RecommendedVideos;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import SidebarRow from './SidebarRow';
+import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
+        <div className = "sidebar">
              <h1>I am a Sidebar</h1>
+             <SidebarRow title = "Home"/>
+             <SidebarRow title = "Trending"/>
+             <SidebarRow title = "Subscription"/>
+             <hr/>
+             <SidebarRow title = "Library"/>
+             <SidebarRow title = "History"/>
+             <SidebarRow title = "Your videos"/>
+             <SidebarRow title = "Watch Later"/>
+             <SidebarRow title = "Liked videos"/>
+             <SidebarRow title = "Show more"/>
+             <hr/>
         </div>
     )
 }
