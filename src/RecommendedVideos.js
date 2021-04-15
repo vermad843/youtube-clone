5bac3ba29dbf2ed8dc117dde59295676f4a1df46
vermad843 <vishnuverma7524@gmail.com>
2021-04-15 20:28:52 +0530

    VideoCard component

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './RecommendedVideos.css'
+import Video from './VideoCard';
 
 
 function RecommendedVideos() {
     return (
         <div className = "recommendedVideos">
             <h1>I am RecommendedVideos</h1>
+            <div className = "recommendedVideos__videos">
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+                <Video/>
+            </div>
         </div>
     );
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoCard.css';
+
+function VideoCard() {
+    return (
+        <div>
+           <h1>I am a VideoCard Component</h1> 
+        </div>
+    )
+}
+
+export default VideoCard
