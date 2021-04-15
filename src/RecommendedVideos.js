2a53ceb3cea900493dc2b8d49fbd2a0bc73215d3
vermad843 <vishnuverma7524@gmail.com>
2021-04-15 21:00:39 +0530

    image on page(videocard)

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -6,9 +6,16 @@ import Video from './VideoCard';
 function RecommendedVideos() {
     return (
         <div className = "recommendedVideos">
-            <h1>I am RecommendedVideos</h1>
+            <h1>Recommended</h1>
             <div className = "recommendedVideos__videos">
-                <Video/>
+                <Video
+                   title = "Happy Birthday by Maninder butter"
+                   views = "2.3M Views"
+                   timestamp = "3 days ago"
+                   channelImage = "https://avatars.githubusercontent.com/u/51009293?s=60&v=4"
+                   channel = "EverythingMusic"
+                   image = "https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw"
+                   />
                 <Video/>
                 <Video/>
                 <Video/>
