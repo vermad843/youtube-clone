8059ab51f40af5dffffb6895e3d25aabe8d421fa
vermad843 <vishnuverma7524@gmail.com>
2021-04-15 20:46:24 +0530

    VideoCard Structure

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import './VideoCard.css';
+// import './VideoCard.css';
+import Avatar from '@material-ui/core/Avatar';
 
-function VideoCard() {
+
+function VideoCard({image,title,channel,views, timestamp, channelImage}) {
     return (
-        <div>
-           <h1>I am a VideoCard Component</h1> 
+        <div className = "videoCard">
+           <img
+              src = {image}
+              alt =""
+             />
+            <div className = "videoCard__info">
+                <Avatar
+                    className = "videoCard__avatar"
+                    alt ={channel}
+                    src = {channelImage}
+                  />
+                  <div className = "video__text">
+                      <h4>{title}</h4>
+                      <p>{channel}</p>
+                      <p>
+                          {views} * {timestamp}
+                      </p>
+                  </div>
+            </div>
         </div>
     )
 }
 
-export default VideoCard
+export default VideoCard;
