19f94d442c6991a7ae43a00f0a7c9011885f7416
vermad843 <vishnuverma7524@gmail.com>
2021-04-15 20:10:39 +0530

    more sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,22 +3,28 @@ import SidebarRow from './SidebarRow';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import HomeIcon from '@material-ui/icons/Home';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAlt'
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMore'
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className = "sidebar">
-             <SidebarRow Icon= {HomeIcon} title = "Home"/>
+             <SidebarRow selected Icon= {HomeIcon} title = "Home"/>
              <SidebarRow Icon = {WhatshotIcon} title = "Trending"/>
              <SidebarRow Icon = {SubscriptionsIcon} title = "Subscription"/>
-             {/* <hr/> */}
-             {/* <SidebarRow title = "Library"/>
-             <SidebarRow title = "History"/>
-             <SidebarRow title = "Your videos"/>
-             <SidebarRow title = "Watch Later"/>
-             <SidebarRow title = "Liked videos"/>
-             <SidebarRow title = "Show more"/>
-             <hr/> */}
+             <hr/>
+             <SidebarRow Icon = {VideoLibraryIcon} title = "Library"/>
+             <SidebarRow Icon = {HistoryIcon} title = "History"/>
+             <SidebarRow Icon = {OndemandVideoIcon} title = "Your videos"/>
+             <SidebarRow Icon = {WatchLaterIcon} title = "Watch Later"/>
+             <SidebarRow Icon = {ThumbUpAltOutlinedIcon} title = "Liked videos"/>
+             <SidebarRow Icon = {ExpandMoreOutlinedIcon} title = "Show more"/>
+             <hr/>
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './SidebarRow.css';
 
-function SidebarRow({Icon ,title}) {
+function SidebarRow({selected ,Icon ,title}) {
     return (
-        <div className = "sidebarRow">
-            <Icon className = "sidebarRow__icon"/>
+        <div className = {`sidebarRow ${selected && "selected"}`}>
+            <Icon  className = "sidebarRow__icon"/>
             <h2 className = "sidebarRow__title">{title}</h2>
         </div>
     )
