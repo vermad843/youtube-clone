b5870edc1d737731538a25c4c7fb0a91d7522f76
vermad843 <vishnuverma7524@gmail.com>
2021-04-16 10:22:48 +0530

    Filter icon

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+import './SearchPage.css';
 
 function SearchPage() {
     return (
-        <div>
-            <h1>I am a SearchPage Component</h1>
+        <div className = "searchPage">
+            <div className = "searchPage__filter">
+                <TuneOutlinedIcon/>
+            </div>
         </div>
     )
 }
