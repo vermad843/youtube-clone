9098095a93d0949f58ed7497ac735df75347e9af
vermad843 <vishnuverma7524@gmail.com>
2021-04-16 11:16:43 +0530

    subscriber count card

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 import './SearchPage.css';
+import ChannelRow from './ChannelRow';
 
 function SearchPage() {
     return (
@@ -9,6 +10,15 @@ function SearchPage() {
                 <TuneOutlinedIcon/>
                 <h2>Filter</h2>
             </div>
+           <hr/>
+           <ChannelRow
+              image = "//yt3.ggpht.com/ytc/AAUvwnjC4gv6dAWxk-jb4tlSp1cdfnoe2XmN1yPhK2x74g=s176-c-k-c0x00ffffff-no-rj-mo"
+              channel = "EverythingMusic"
+              verified
+              subs="659k"
+              noOfVideos={382}
+              description = "Amazing Songs You can find here"
+             />
         </div>
     )
 }
